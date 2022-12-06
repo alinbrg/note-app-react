a42d5b132d605175a0394080f610c43dacedaa92
alinbrg <alina.bregvadze@gmail.com>
2022-12-06 15:28:32 +0400

    scrimba note react app start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Sidebar from "./components/Sidebar";
+import Editor from "./components/Editor";
+import { data } from "./data";
+import Split from "react-split";
+import { nanoid } from "nanoid";
+import "react-mde/lib/styles/css/react-mde-all.css";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/**
+ * Challenge: Spend 10-20+ minutes reading through the code
+ * and trying to understand how it's currently working. Spend
+ * as much time as you need to feel confident that you
+ * understand the existing code (although you don't need
+ * to fully understand everything to move on)
+ */
+
+export default function App() {
+	const [notes, setNotes] = useState([]);
+	const [currentNoteId, setCurrentNoteId] = useState(
+		(notes[0] && notes[0].id) || ""
+	);
+
+	function createNewNote() {
+		const newNote = {
+			id: nanoid(),
+			body: "# Type your markdown note's title here",
+		};
+		setNotes((prevNotes) => [newNote, ...prevNotes]);
+		setCurrentNoteId(newNote.id);
+	}
+
+	function updateNote(text) {
+		setNotes((oldNotes) =>
+			oldNotes.map((oldNote) => {
+				return oldNote.id === currentNoteId
+					? { ...oldNote, body: text }
+					: oldNote;
+			})
+		);
+	}
+
+	function findCurrentNote() {
+		return (
+			notes.find((note) => {
+				return note.id === currentNoteId;
+			}) || notes[0]
+		);
+	}
+
+	return (
+		<main>
+			{notes.length > 0 ? (
+				<Split sizes={[30, 70]} direction="horizontal" className="split">
+					<Sidebar
+						notes={notes}
+						currentNote={findCurrentNote()}
+						setCurrentNoteId={setCurrentNoteId}
+						newNote={createNewNote}
+					/>
+					{currentNoteId && notes.length > 0 && (
+						<Editor currentNote={findCurrentNote()} updateNote={updateNote} />
+					)}
+				</Split>
+			) : (
+				<div className="no-notes">
+					<h1>You have no notes</h1>
+					<button className="first-note" onClick={createNewNote}>
+						Create one now
+					</button>
+				</div>
+			)}
+		</main>
+	);
 }
-
-export default App;
